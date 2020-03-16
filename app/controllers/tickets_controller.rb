72da4c6198273f25c6ce40f9c71aaab9c98b83f3
Andrea Jansen <andrea.jansen@shadowhealth.com>
2020-03-16 00:46:59 -0400

    added email to user after ticket purchase

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -15,9 +15,20 @@ class TicketsController < ApplicationController
 
   def create
     @ticket = Ticket.new(ticket_params)
+    
 
-    @ticket.save!
-    redirect_to @ticket
+    respond_to do |format|
+      if @ticket.save
+        # Tell the UserMailer to send a welcome email after save
+        TicketMailer.with(ticket: @ticket).welcome_email.deliver_later
+ 
+        format.html { redirect_to(@ticket, notice: 'Ticket was successfully created.') }
+        format.json { render json: @ticket, status: :created, location: @ticket }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @ticket.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
